b9096a0daff658508c8f312c97f951f8a8dc1640
vamshi-beeram <vamshibiram8@gmail.com>
2021-01-26 19:28:27 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import Greet from './components/Greet'
+import Hello from './components/Hello'
+import Travello from './components/Travello/Travello'
+function App() {
+  return (
+    <div className="App">
+     <Travello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,8 @@
+import React,{ Component } from 'react';
+ class Hello extends Component
+ {
+     render(){
+         return <h1> hello vamshi </h1>
+     }
+ } 
+ export default Hello
